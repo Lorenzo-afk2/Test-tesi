7eca3fd941793ad64d357c7c92c64b5bc869d343
Lorenzo-afk2 <lorenzopiga2020@gmail.com>
2026-09-23 12:37:19 +0200

    Update app.py

--- app.py
+++ app.py
@@ -5,13 +5,26 @@ st.title("Impostazioni Parametri per Categoria")
 # 1. Creazione del menu a tendina
 categoria = st.selectbox(
     "Seleziona la Categoria del Prodotto:",
-    ["Scegli un'opzione...", "Fresco (Es. Insalata)", "Congelato (Es. Hamburger)", "Secco (Es. Bicchieri)"]
+    ["Scegli un'opzione...", "Congelato (Es. Hamburger)", "Fresco (Es. Insalata)","Secco (Es. Bicchieri)"]
 )
 
 # 2. Logica condizionale (if/elif)
 # Se l'utente non ha ancora scelto nulla, mostriamo un messaggio
 if categoria == "Scegli un'opzione...":
     st.info("Seleziona una categoria per visualizzare i parametri logistici associati.")
+
+
+# Se seleziona "Congelato"
+elif categoria == "Congelato (Es. Hamburger)":
+    st.write("### Parametri Operativi - Cella Negativa")
+    # Costo mantenimento altissimo (consumo elettrico)
+    costo_mantenimento = st.number_input("Costo di Mantenimento (H) €", value=8.00, min_value=0.0)
+    # Livello di servizio massimo (prodotto vitale)
+    livello_servizio = st.selectbox("Livello di Servizio Desiderato:", ["95%", "99%", "99.9% (Consigliato)"], index=2)
+    
+    st.error("Prodotto Core: La rottura di stock non è tollerata. Verrà applicato il Safety Stock massimo (Z=3.09).")
+
+
 
 # Se seleziona "Fresco"
 elif categoria == "Fresco (Es. Insalata)":
@@ -23,15 +36,6 @@ elif categoria == "Fresco (Es. Insalata)":
     
     st.warning("Attenzione: I prodotti freschi hanno un alto rischio di obsolescenza. L'algoritmo terrà l'EOQ molto basso.")
 
-# Se seleziona "Congelato"
-elif categoria == "Congelato (Es. Hamburger)":
-    st.write("### Parametri Operativi - Cella Negativa")
-    # Costo mantenimento altissimo (consumo elettrico)
-    costo_mantenimento = st.number_input("Costo di Mantenimento (H) €", value=8.00, min_value=0.0)
-    # Livello di servizio massimo (prodotto vitale)
-    livello_servizio = st.selectbox("Livello di Servizio Desiderato:", ["95%", "99%", "99.9% (Consigliato)"], index=2)
-    
-    st.error("Prodotto Core: La rottura di stock non è tollerata. Verrà applicato il Safety Stock massimo (Z=3.09).")
 
 # Se seleziona "Secco"
 elif categoria == "Secco (Es. Bicchieri)":
