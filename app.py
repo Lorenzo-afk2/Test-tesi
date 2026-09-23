3cac0a9b7e373a6a30ab7caf49c5e332a97cc50d
Lorenzo-afk2 <lorenzopiga2020@gmail.com>
2026-09-23 14:22:13 +0200

    Update app.py

--- app.py
+++ app.py
@@ -1,19 +1,10 @@
 import streamlit as st
 
-st.title("Ottimizzazione Scorte (EOQ)")
-st.write("Benvenuto nel sistema di calcolo logistico.")
-
-st.divider() # <--- Prima riga per separare l'intestazione
-
-# --- ZONA INPUT ---
-st.subheader("1. Inserimento Dati")
-st.file_uploader("Carica Storico Vendite", type=["csv"])
-
-st.write("---") # <--- Seconda riga per separare gli input dai risultati
-
-# --- ZONA OUTPUT ---
-st.subheader("2. Risultati Calcolati")
-st.metric(label="Lotto Economico (EOQ)", value="150 colli")
+# La parola chiave type accetta una lista di formati
+file_dati = st.file_uploader(
+    "Carica lo storico vendite:", 
+    type=["csv", "xlsx", "xls"]
+)
 
 
 
